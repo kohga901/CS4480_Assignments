09895dfb89308a169e80ed4a5b0074a5f94b1c5f
Khongor <khongor0901@gmail.com>
2025-03-27 19:12:04 -0600

    Kinda works now

--- Controller.py
+++ Controller.py
@@ -2,6 +2,8 @@
 This is a POX controller.
 """
 from pox.core import core
+import pox
+log = core.getLogger()
 import pox.openflow.libopenflow_01 as of
 from mininet.net import Mininet
 from mininet.node import Controller, RemoteController, OVSKernelSwitch
@@ -17,7 +19,6 @@ from pox.lib.revent import EventHalt
 
 import pox.openflow.libopenflow_01 as of
 
-log = core.getLogger()
 
 class Controller(object):
     """ 
@@ -25,115 +26,141 @@ class Controller(object):
     
     It takes in an incoming packet, and decides what to do with it.
     """
-def __init__(self, connection):
-    # Initialize the connection from this controller to the switch.
-    self.connection = connection
+    def __init__(self, connection):
+        # Initialize the connection from this controller to the switch.
+        self.connection = connection
 
-    # Should I make a forwarding table here?
+        # Should I make a forwarding table here?
 
-    # Add a listener to the connection so that when a packet comes in to the switch, the controller is notified.
-    connection.addListeners(self)
+        # Add a listener to the connection so that when a packet comes in to the switch, the controller is notified.
+        connection.addListeners(self)
 
-    # Log the initializer.
-    log.debug("Initializing the controller.")
+        core.openflow.addListenerByName("PacketIn", self.handle_PacketIn)
+
+        # Log the initializer.
+        log.info("Initializing the controller.")
 
 
-def handle_ARP_Packets(self, event):
-    """
-    This method handles the packets that come into the controller.
-    When a packet is sent to the controller, it actually comes in as an event. 
-    Then you have to parse that event to turn it into a packet.
-    """
-    # Getting the port of the event.
-    inport = event.port 
-    # Extracting the packet from the event.
-    packet = event.parsed
-    # Checking if the packet is an arp request packet.
-    arp_packet = packet.find('arp')
-    if arp_packet:
-        log.debug("ARP request received by controller.")
+    def handle_PacketIn(self, event):
+        """
+        This method handles the packets that come into the controller.
+        When a packet is sent to the controller, it actually comes in as an event. 
+        Then you have to parse that event to turn it into a packet.
+        """
+        # Getting the port of the event.
+        inport = event.port 
+        # Extracting the packet from the event.
+        packet = event.parsed
+        # Checking if the packet is an arp request packet.
+        arp_packet = packet.find('arp')
+        if not arp_packet: 
+            forward_Packet(event)
+            
+        log.info("ARP request received by controller.")
+        status = True
 
-    status = True
+        # Check if the packet is an ARP REQUEST.
+        if (arp_packet.opcode == arp.REQUEST):
+            # Build an ARP REPLY packet.
+            arp_reply = arp()
+            arp_reply.hwtype = arp_packet.hwtype
+            arp_reply.prototype = arp_packet.prototype
+            arp_reply.hwlen = arp_packet.hwlen
+            arp_reply.protolen = arp_packet.protolen
+            arp_reply.opcode = arp.REPLY
+            arp_reply.hwdst = arp_packet.hwsrc
+            arp_reply.protodst = arp_packet.protosrc
+            arp_reply.protosrc = arp_packet.protodst
 
-    # Check if the packet is an ARP REQUEST.
-    if (arp_packet.opcode == arp.REQUEST):
-        # Build an ARP REPLY packet.
-        arp_reply = arp()
-        arp_reply.hwtype = arp_packet.hwtype
-        arp_reply.prototype = arp_packet.prototype
-        arp_reply.hwlen = arp_packet.hwlen
-        arp_reply.protolen = arp_packet.protolen
-        arp_reply.opcode = arp.REPLY
-        arp_reply.hwdst = arp_packet.hwsrc
-        arp_reply.protodst = arp_packet.protosrc
-        arp_reply.protosrc = arp_packet.protodst
+            # If the ARP REQUEST's dst IP is 10.0.0.10, return the mac address 00:00:00:00:00:05.
+            if (arp_packet.protodst == IPAddr("10.0.0.10")):
+                if (status):
+                    # check if just assigning it like this works or not
+                    arp_reply.hwsrc =  EthAddr("00:00:00:00:00:05")
+            if (arp_packet.protodst == IPAddr("10.0.0.1")):
+                arp_reply.hwsrc = EthAddr("00:00:00:00:00:01")
+            if (arp_packet.protodst == IPAddr("10.0.0.2")):
+                arp_reply.hwsrc = EthAddr("00:00:00:00:00:02")
+            if (arp_packet.protodst == IPAddr("10.0.0.3")):
+                arp_reply.hwsrc = EthAddr("00:00:00:00:00:03")
+            if (arp_packet.protodst == IPAddr("10.0.0.4")):
+                arp_reply.hwsrc = EthAddr("00:00:00:00:00:04")
+        
+            e = ethernet(type=packet.type, src=event.connection.eth_addr,
+                            dst=arp_packet.hwsrc)
+            e.payload = arp_reply
 
-        if (arp_packet.protodst == IPAddr("10.0.0.10")):
-            if (status):
-                # check if just assigning it like this works or not
-                arp_reply.hwsrc =  EthAddr("00:00:00:00:00:05")
-        if (arp_packet.protodst == IPAddr("10.0.0.1")):
-            arp_reply.hwsrc = EthAddr("00:00:00:00:00:01")
-        if (arp_packet.protodst == IPAddr("10.0.0.2")):
-            arp_reply.hwsrc = EthAddr("00:00:00:00:00:02")
-        if (arp_packet.protodst == IPAddr("10.0.0.3")):
-            arp_reply.hwsrc = EthAddr("00:00:00:00:00:03")
-        if (arp_packet.protodst == IPAddr("10.0.0.4")):
-            arp_reply.hwsrc = EthAddr("00:00:00:00:00:04")
-       
-        e = ethernet(type=packet.type, src=event.connection.eth_addr,
-                        dst=arp_packet.hwsrc)
-        e.payload = arp_reply
+            log.info("Answering ARP for %s" % (str(arp_reply.protodst)))
+            msg = of.ofp_packet_out()
+            msg.data = e.pack()
+            msg.actions.append(of.ofp_action_output(port =
+                                                    of.OFPP_IN_PORT))
+            msg.in_port = inport
+            event.connection.send(msg)
 
-        log.info("Answering ARP for %s" % (str(arp_reply.protosrc)))
-        msg = of.ofp_packet_out()
-        msg.data = e.pack()
-        msg.actions.append(of.ofp_action_output(port =
-                                                of.OFPP_IN_PORT))
-        msg.in_port = inport
-        event.connection.send(msg)
+            log.info("ARP Reply sent. The MAC address sent was: %s" % (str(arp_reply.hwsrc)))
 
-        add_flow_rules(self)
+            # Creating a match rule for the switch.
+            msg = of.ofp_flow_mod()
+            msg.match.in_port = 1
+            msg.match.dl_type = 0x0800
+            msg.match.nw_dst = IPAddr("10.0.0.10")
+            msg.match.nw_src = IPAddr("10.0.0.1")
+            msg.actions.append(of.ofp_action_nw_addr.set_dst(IPAddr("10.0.0.5")))
 
-    # msg = of.ofp_flow_mod()
-    # msg.match.in_port = 1
-    # msg.match.dl_type = 0x0800
-    # msg.match.nw_dst = IPAddr("10.0.0.1")
-    # msg.match.nw_src = IPAddr("10.0.0.10")
-    # msg.actions.append(of.ofp_action_nw_addr.set_dst(IPAddr("10.0.0.5")))
+            # Telling the switch to go throughout port 5.
+            out_action = of.ofp_action_output(port = 5)
+            msg.actions.append(out_action)
+            self.connection.send(msg)
 
-    # self.connection.send(msg)
-    # msg = of.ofp_flow_mod()
-    # msg.match.in_port = 1
-    # msg.match.dl_type = 0x0800
-    # msg.match.nw_dst = IPAddr("10.0.0.5")
-    # msg.match.nw_src = IPAddr("10.0.0.1")
-    # msg.actions.append(of.ofp_action_nw_addr.set_src(IPAddr("10.0.0.10")))
+            msg = of.ofp_flow_mod()
+            msg.match.in_port = 5
+            msg.match.dl_type = 0x0800
+            msg.match.nw_dst = IPAddr("10.0.0.1")
+            msg.match.nw_src = IPAddr("10.0.0.5")
+            msg.actions.append(of.ofp_action_nw_addr.set_src(IPAddr("10.0.0.10")))
+            out_action = of.ofp_action_output(port = 1)
+            msg.actions.append(out_action)
+            self.connection.send(msg)
+        
+        else:
+            return
+        
+    def forward_Packet(self, event):
+        msg = of.ofp_action_output(port = 5)
+        self.connection.send(msg)
+        
+    # def add_flow_rules(self, event):
+        # msg = of.ofp_flow_mod()
+        # msg.match.in_port = 1
+        # msg.match.dl_type = 0x0800
+        # msg.match.nw_dst = IPAddr("10.0.0.1")
+        # msg.match.nw_src = IPAddr("10.0.0.10")
+        # msg.actions.append(of.ofp_action_nw_addr.set_dst(IPAddr("10.0.0.5")))
 
-    # self.connection.send(msg)
+        # log.info("Logging.")
 
-def add_flow_rules(self):
-    msg = of.ofp_flow_mod()
-    msg.match.in_port = 1
-    msg.match.dl_type = 0x0800
-    msg.match.nw_dst = IPAddr("10.0.0.1")
-    msg.match.nw_src = IPAddr("10.0.0.10")
-    msg.actions.append(of.ofp_action_nw_addr.set_dst(IPAddr("10.0.0.5")))
+        # self.connection.send(msg)
+        # msg = of.ofp_flow_mod()
+        # msg.match.in_port = 1
+        # msg.match.dl_type = 0x0800
+        # msg.match.nw_dst = IPAddr("10.0.0.5")
+        # msg.match.nw_src = IPAddr("10.0.0.1")
+        # msg.actions.append(of.ofp_action_nw_addr.set_src(IPAddr("10.0.0.10")))
 
-    self.connection.send(msg)
-    msg = of.ofp_flow_mod()
-    msg.match.in_port = 1
-    msg.match.dl_type = 0x0800
-    msg.match.nw_dst = IPAddr("10.0.0.5")
-    msg.match.nw_src = IPAddr("10.0.0.1")
-    msg.actions.append(of.ofp_action_nw_addr.set_src(IPAddr("10.0.0.10")))
+        # log.info("Logging.")
 
-    self.connection.send(msg)
+        # self.connection.send(msg)
 
 def launch():
     """
-    This method is called when the controller is set up. It's like a main __init__ method. 
+    This method is called when the controller is set up. It's like a main() method. 
     """
+    log.info("Logging launch.")
+
+    def connection_up(event):
+        Controller(event.connection)
 
     # Putting the controller up.
-    core.registerNew(Controller)  
+    core.openflow.addListenerByName("ConnectionUp", connection_up)
+
